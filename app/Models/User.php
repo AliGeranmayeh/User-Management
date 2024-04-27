01fb5beef7ebcccdc830abbef9b0579b26040fd2
Ali Geranmayeh <51305499+AliGeranmayeh@users.noreply.github.com>
2024-04-27 11:48:44 +0330

    add roles table and update users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -21,6 +22,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'personal_code',
+        'image',
+        'role_id'
     ];
 
     /**
@@ -30,15 +34,9 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    function setPasswordAttribute($value) {
+        $this->attributes['password'] =Hash::make($value);
+    }
 }
