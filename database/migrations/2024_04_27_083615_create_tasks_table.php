8744c88cfdfb786737317c6c14e87edba481d364
Ali Geranmayeh <51305499+AliGeranmayeh@users.noreply.github.com>
2024-04-27 12:17:08 +0330

    add tasks table

---
+++ database/migrations/2024_04_27_083615_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('weight')->default(0);
+            $table->string('status');
+            $table->foreignId('goal_id')->constrained('goals')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
