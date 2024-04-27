339bcd680e6118e510600e715df6293aa5513c0c
Ali Geranmayeh <51305499+AliGeranmayeh@users.noreply.github.com>
2024-04-27 14:10:12 +0330

    complete index response

--- app/Helpers/Response/UserResponse.php
+++ app/Helpers/Response/UserResponse.php
@@ -2,11 +2,16 @@
 
 namespace App\Helpers\Response;
 
+use App\Helpers\Response\MainResponse\ApiResponse;
+use Illuminate\Http\Response;
+
 
 trait UserResponse
 {
-    private function indexResponse($users) {
+    use ApiResponse;
 
+    private function indexResponse($users) {
+       return $this->successWithData('ok', $users , Response::HTTP_OK);
     }
 
     private function storeResponse($user) {
