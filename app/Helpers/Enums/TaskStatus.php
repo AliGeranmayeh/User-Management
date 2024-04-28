a6aaf0c8649b0343bed13e567eb8f0fc9ad7d768
Ali Geranmayeh <51305499+AliGeranmayeh@users.noreply.github.com>
2024-04-28 13:24:20 +0330

    create weight and status enum for task

---
+++ app/Helpers/Enums/TaskStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Helpers\Enums;
+
+
+class TaskStatus
+{
+    const PENDING = 0;
+    const FINISHED = 1;
+    const IN_PROGRESS = 0.5;
+    const STOP = 0;
+}
---
+++ app/Helpers/Enums/TaskWeight.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers\Enums;
+
+
+class TaskWeight
+{
+    const MINIMAL = 1;
+    const LOW = 2;
+    const MEDIUM = 3;
+    const HIGH = 4;
+    const CRITICAL = 5;
+
+}
